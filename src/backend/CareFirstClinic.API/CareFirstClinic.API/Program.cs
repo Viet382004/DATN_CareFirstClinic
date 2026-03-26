9ed1ab4e3d0efd5c3e909ebe669542057005e17a
Viet382004 <viet01386@gmail.com>
2026-03-26 17:05:16 +0700

    fetch: off seed data ro deploy

--- src/backend/CareFirstClinic.API/CareFirstClinic.API/Program.cs
+++ src/backend/CareFirstClinic.API/CareFirstClinic.API/Program.cs
@@ -135,11 +135,11 @@ var app = builder.Build();
 //     }
 // }
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
