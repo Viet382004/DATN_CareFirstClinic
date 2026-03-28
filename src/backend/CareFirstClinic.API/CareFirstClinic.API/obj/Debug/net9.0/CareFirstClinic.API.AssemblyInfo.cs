132f32841de1e1d3f6dd21b598e733d75a785a99
Viet382004 <viet01386@gmail.com>
2026-03-28 22:34:47 +0700

    update repo

--- src/backend/CareFirstClinic.API/CareFirstClinic.API/obj/Debug/net9.0/CareFirstClinic.API.AssemblyInfo.cs
+++ src/backend/CareFirstClinic.API/CareFirstClinic.API/obj/Debug/net9.0/CareFirstClinic.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareFirstClinic.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d149697f6cfae803fae00195bf551ea1057d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e2f97e55e22bbdeb117e7eaf6c99c8c722db99")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareFirstClinic.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareFirstClinic.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
